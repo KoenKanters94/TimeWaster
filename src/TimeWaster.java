41bdf07d2950c63573a9e0ef598c3c2d096a561f
Nathan Cassee <nathancassee@gmail.com>
2014-11-28 00:06:29 +0100

    Koen's sloppy implementation with an average of 260-275 ms per attempt over 300 attempts

--- src/TimeWaster.java
+++ src/TimeWaster.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,55 +11,56 @@ import java.util.List;
  *
  * @author Koenkk
  */
-public class TimeWaster {
+public class TimeWaster
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String s = "typewriter";
-        double possibilities = Math.pow(s.length(), 26);
-        System.out.printf("There are %.0f possibilities\n", possibilities);
-        final long startTime = System.currentTimeMillis();
-        long attempts = 0;
+        int runningTotal = 0;
         
-        for (int i = 0; i < Integer.MAX_VALUE; i++) {
-            s = shuffle(s);
-            
-            if (s.equals("eeiprrttwy")) {
-                System.out.println(s);
-                printWastedTime(startTime, attempts);
-                break;
+        for (int j = 0; j < 300; j++)
+        {
+            final long startTime = System.currentTimeMillis();
+
+            for (int i = 0; i < Integer.MAX_VALUE; i++)
+            {
+                s = shuffle(s);
+
+                if (s.equals("eeiprrttwy"))
+                {
+                    runningTotal += System.currentTimeMillis() - startTime;
+                    break;
+                }
+
+                if (i == Integer.MAX_VALUE)
+                {
+                    i = 0;
+                }
             }
-            
-            if (i == Integer.MAX_VALUE) {
-                 i = 0;
-                 printWastedTime(startTime, attempts);
-                 System.out.println("Max integer reached!");
-            }          
-            
-            attempts++;
         }
+        
+        System.out.println("Spent " + runningTotal + "ms for 300 tries. Average is: " + runningTotal / 300 + "ms");
     }
-    
-    public static void printWastedTime(long beginTime, long attempts) {
-        long wastedTime = System.currentTimeMillis() - beginTime;
-        System.out.println("You wasted: " + wastedTime + " milliseconds (Attempts: " + attempts + ")");
-    }
-  
-    
-    public static String shuffle(String input){
+
+    public static String shuffle(String input)
+    {
         List<Character> characters = new ArrayList<Character>();
-        for(char c:input.toCharArray()){
+        for (char c : input.toCharArray())
+        {
             characters.add(c);
         }
         StringBuilder output = new StringBuilder(input.length());
-        while(characters.size()!=0){
-            int randPicker = (int)(Math.random()*characters.size());
+        while (characters.size() != 0)
+        {
+            int randPicker = (int) (Math.random() * characters.size());
             output.append(characters.remove(randPicker));
         }
-        
+
         return output.toString();
     }
-    
+
 }
